6364bb3ec019f6fe3ffd4325b00c44f6b8a23e15
Duy Thai <sb23092002@gmail.com>
2024-02-22 14:25:49 +0700

    Debug

--- src/main/java/com/vcc/adopt/training/bigdata/spark/SparkHBase.scala
+++ src/main/java/com/vcc/adopt/training/bigdata/spark/SparkHBase.scala
@@ -32,158 +32,6 @@ object SparkHBase {
   private val output4 = ConfigPropertiesLoader.getYamlConfig.getProperty("output4")
   private val outputFilePath = ConfigPropertiesLoader.getYamlConfig.getProperty("outputFilePath")
 
-  private def createDataFrameAndPutToHDFS(): Unit = {
-    println(s"----- Make person info dataframe then write to parquet at ${personInfoLogPath} ----")
-
-    // tạo person-info dataframe và lưu vào HDFS
-    val data = Seq(
-      Row(1L, "Alice", 25),
-      Row(2L, "Bob", 30),
-      Row(3L, "Charlie", 22),
-      Row(4L, "Yorn", 22)
-    )
-
-    //    val schema = StructType(Seq(
-    //      StructField("personId", LongType, nullable = true),
-    //      StructField("name", StringType, nullable = true),
-    //      StructField("age", IntegerType, nullable = true)
-    //    ))
-    val schema = StructType(Seq(
-      StructField("timeCreate", TimestampType, nullable = true),
-      StructField("cookieCreate", TimestampType, nullable = true),
-      StructField("browserCode", IntegerType, nullable = true),
-      StructField("browserVer", StringType, nullable = true),
-      StructField("osCode", IntegerType, nullable = true),
-      StructField("osVer", StringType, nullable = true),
-      StructField("ip", LongType, nullable = true),
-      StructField("locId", IntegerType, nullable = true),
-      StructField("domain", StringType, nullable = true),
-      StructField("siteId", IntegerType, nullable = true),
-      StructField("cId", IntegerType, nullable = true),
-      StructField("path", StringType, nullable = true),
-      StructField("referer", StringType, nullable = true),
-      StructField("guid", LongType, nullable = true),
-      StructField("flashVersion", StringType, nullable = true),
-      StructField("jre", StringType, nullable = true),
-      StructField("sr", StringType, nullable = true),
-      StructField("sc", StringType, nullable = true),
-      StructField("geographic", IntegerType, nullable = true),
-      StructField("category", IntegerType, nullable = true)
-    ))
-
-    val df = spark.createDataFrame(spark.sparkContext.parallelize(data), schema)
-    df.show()
-    df.write
-      .mode("overwrite") // nếu file này đã tồn tại trước đó, sẽ ghi đè
-      .parquet(personInfoLogPath)
-
-    // tạo person-id-list và lưu vào HDFS
-    df.select("personId")
-      .write
-      .mode("overwrite")
-      .parquet(personIdListLogPath)
-
-
-  }
-
-  private def readHDFSThenPutToHBase(): Unit = {
-    println("----- Read person-info.parquet on HDFS then put to table person:person-info ----")
-    //    var df = spark.read.parquet(personInfoLogPath)
-    val schema = StructType(Seq(
-      StructField("timeCreate", TimestampType, nullable = true),
-      StructField("cookieCreate", TimestampType, nullable = true),
-      StructField("browserCode", IntegerType, nullable = true),
-      StructField("browserVer", StringType, nullable = true),
-      StructField("osCode", IntegerType, nullable = true),
-      StructField("osVer", StringType, nullable = true),
-      StructField("ip", LongType, nullable = true),
-      StructField("locId", IntegerType, nullable = true),
-      StructField("domain", StringType, nullable = true),
-      StructField("siteId", IntegerType, nullable = true),
-      StructField("cId", IntegerType, nullable = true),
-      StructField("path", StringType, nullable = true),
-      StructField("referer", StringType, nullable = true),
-      StructField("guid", LongType, nullable = true),
-      StructField("flashVersion", StringType, nullable = true),
-      StructField("jre", StringType, nullable = true),
-      StructField("sr", StringType, nullable = true),
-      StructField("sc", StringType, nullable = true),
-      StructField("geographic", IntegerType, nullable = true),
-      StructField("category", IntegerType, nullable = true)
-    ))
-    var df = spark.read
-      .schema(schema)
-      .option("delimiter", "\t")
-      .csv(test)
-    df = df
-      .withColumn("country", lit("US"))
-      .repartition() // chia dataframe thành 5 phân vùng, mỗi phân vùng sẽ được chạy trên một worker (nếu không chia mặc định là 200)
-
-    val batchPutSize = 100 // để đẩy dữ liệu vào hbase nhanh, thay vì đẩy lẻ tẻ từng dòng thì ta đẩy theo lô, như ví dụ là cứ 100 dòng sẽ đẩy 1ần
-    df.foreachPartition((rows: Iterator[Row]) => {
-      // tạo connection hbase buộc phải tạo bên trong mỗi partition (không được tạo bên ngoài). Tối ưu hơn sẽ dùng connectionPool để reuse lại connection trên các worker
-      val hbaseConnection = HBaseConnectionFactory.createConnection()
-      try {
-        val table = hbaseConnection.getTable(TableName.valueOf("person", "person_info"))
-        val puts = new util.ArrayList[Put]()
-        for (row <- rows) {
-          val timeCreate = row.getAs[java.sql.Timestamp]("timeCreate").getTime
-          val cookieCreate = row.getAs[java.sql.Timestamp]("cookieCreate").getTime
-          val browserCode = row.getAs[Int]("browserCode")
-          val browserVer = row.getAs[String]("browserVer")
-          val osCode = row.getAs[Int]("osCode")
-          val osVer = Option(row.getAs[String]("osVer")).getOrElse(" ")
-          val ip = row.getAs[Long]("ip")
-          val locId = row.getAs[Int]("locId")
-          val domain = row.getAs[String]("domain")
-          val siteId = row.getAs[Int]("siteId")
-          val cId = row.getAs[Int]("cId")
-          val path = row.getAs[String]("path")
-          val referer = row.getAs[String]("referer")
-          val guid = row.getAs[Long]("guid")
-          val flashVersion = row.getAs[String]("flashVersion")
-          val jre = row.getAs[String]("jre")
-          val sr = row.getAs[String]("sr")
-          val sc = row.getAs[String]("sc")
-          val geographic = row.getAs[Int]("geographic")
-          val category = row.getAs[Int]("category")
-
-          val put = new Put(Bytes.toBytes(timeCreate))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("timeCreate"), Bytes.toBytes(timeCreate))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("cookieCreate"), Bytes.toBytes(cookieCreate))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("browserCode"), Bytes.toBytes(browserCode))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("browserVer"), Bytes.toBytes(browserVer))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("osCode"), Bytes.toBytes(osCode))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("osVer"), Bytes.toBytes(osVer))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("ip"), Bytes.toBytes(ip))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("locId"), Bytes.toBytes(locId))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("domain"), Bytes.toBytes(domain))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("siteId"), Bytes.toBytes(siteId))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("cId"), Bytes.toBytes(cId))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("path"), Bytes.toBytes(path))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("referer"), Bytes.toBytes(referer))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("guid"), Bytes.toBytes(guid))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("flashVersion"), Bytes.toBytes(flashVersion))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("jre"), Bytes.toBytes(jre))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("sr"), Bytes.toBytes(sr))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("sc"), Bytes.toBytes(sc))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("geographic"), Bytes.toBytes(geographic))
-          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("category"), Bytes.toBytes(category))
-
-          puts.add(put)
-          table.put(puts)
-          puts.clear()
-        }
-      } finally {
-        hbaseConnection.close()
-      }
-    })
-  }
-
-
-
-
-
   // ######################################################### START #######################################################
   //  private def readHDFSThenPutToHBase(): Unit = {
   //    // Định nghĩa kiểu dữ liệu của mô hình log
@@ -280,6 +128,146 @@ object SparkHBase {
 
   // ######################################################### END #######################################################
 
+
+  private def createDataFrameAndPutToHDFS(): Unit = {
+    println(s"----- Make person info dataframe then write to parquet at ${personInfoLogPath} ----")
+
+    // tạo person-info dataframe và lưu vào HDFS
+    val data = Seq(
+      Row(1L, "Alice", 25),
+      Row(2L, "Bob", 30),
+      Row(3L, "Charlie", 22),
+      Row(4L, "Yorn", 22)
+    )
+    val schema = StructType(Seq(
+      StructField("timeCreate", TimestampType, nullable = true),
+      StructField("cookieCreate", TimestampType, nullable = true),
+      StructField("browserCode", IntegerType, nullable = true),
+      StructField("browserVer", StringType, nullable = true),
+      StructField("osCode", IntegerType, nullable = true),
+      StructField("osVer", StringType, nullable = true),
+      StructField("ip", LongType, nullable = true),
+      StructField("locId", IntegerType, nullable = true),
+      StructField("domain", StringType, nullable = true),
+      StructField("siteId", IntegerType, nullable = true),
+      StructField("cId", IntegerType, nullable = true),
+      StructField("path", StringType, nullable = true),
+      StructField("referer", StringType, nullable = true),
+      StructField("guid", LongType, nullable = true),
+      StructField("flashVersion", StringType, nullable = true),
+      StructField("jre", StringType, nullable = true),
+      StructField("sr", StringType, nullable = true),
+      StructField("sc", StringType, nullable = true),
+      StructField("geographic", IntegerType, nullable = true),
+      StructField("category", IntegerType, nullable = true)
+    ))
+
+    val df = spark.createDataFrame(spark.sparkContext.parallelize(data), schema)
+    df.show()
+    df.write
+      .mode("overwrite") // nếu file này đã tồn tại trước đó, sẽ ghi đè
+      .parquet(personInfoLogPath)
+
+    // tạo person-id-list và lưu vào HDFS
+    df.select("personId")
+      .write
+      .mode("overwrite")
+      .parquet(personIdListLogPath)
+  }
+
+  private def readHDFSThenPutToHBase(): Unit = {
+    println("----- Read person-info.parquet on HDFS then put to table person:person-info ----")
+    var df = spark.read.parquet(datalog)
+    val schema = StructType(Seq(
+      StructField("timeCreate", TimestampType, nullable = true),
+      StructField("cookieCreate", TimestampType, nullable = true),
+      StructField("browserCode", IntegerType, nullable = true),
+      StructField("browserVer", StringType, nullable = true),
+      StructField("osCode", IntegerType, nullable = true),
+      StructField("osVer", StringType, nullable = true),
+      StructField("ip", LongType, nullable = true),
+      StructField("locId", IntegerType, nullable = true),
+      StructField("domain", StringType, nullable = true),
+      StructField("siteId", IntegerType, nullable = true),
+      StructField("cId", IntegerType, nullable = true),
+      StructField("path", StringType, nullable = true),
+      StructField("referer", StringType, nullable = true),
+      StructField("guid", LongType, nullable = true),
+      StructField("flashVersion", StringType, nullable = true),
+      StructField("jre", StringType, nullable = true),
+      StructField("sr", StringType, nullable = true),
+      StructField("sc", StringType, nullable = true),
+      StructField("geographic", IntegerType, nullable = true),
+      StructField("category", IntegerType, nullable = true)
+    ))
+    var df = spark.read
+      .schema(schema)
+      .option("delimiter", "\t")
+      .csv(test)
+    df = df
+      .withColumn("country", lit("US"))
+      .repartition() // chia dataframe thành 5 phân vùng, mỗi phân vùng sẽ được chạy trên một worker (nếu không chia mặc định là 200)
+
+    val batchPutSize = 100 // để đẩy dữ liệu vào hbase nhanh, thay vì đẩy lẻ tẻ từng dòng thì ta đẩy theo lô, như ví dụ là cứ 100 dòng sẽ đẩy 1ần
+    df.foreachPartition((rows: Iterator[Row]) => {
+      // tạo connection hbase buộc phải tạo bên trong mỗi partition (không được tạo bên ngoài). Tối ưu hơn sẽ dùng connectionPool để reuse lại connection trên các worker
+      val hbaseConnection = HBaseConnectionFactory.createConnection()
+      try {
+        val table = hbaseConnection.getTable(TableName.valueOf("person", "person_info"))
+        val puts = new util.ArrayList[Put]()
+        for (row <- rows) {
+          val timeCreate = row.getAs[java.sql.Timestamp]("timeCreate").getTime
+          val cookieCreate = row.getAs[java.sql.Timestamp]("cookieCreate").getTime
+          val browserCode = row.getAs[Int]("browserCode")
+          val browserVer = row.getAs[String]("browserVer")
+          val osCode = row.getAs[Int]("osCode")
+          val osVer = Option(row.getAs[String]("osVer")).getOrElse(" ")
+          val ip = row.getAs[Long]("ip")
+          val locId = row.getAs[Int]("locId")
+          val domain = row.getAs[String]("domain")
+          val siteId = row.getAs[Int]("siteId")
+          val cId = row.getAs[Int]("cId")
+          val path = row.getAs[String]("path")
+          val referer = row.getAs[String]("referer")
+          val guid = row.getAs[Long]("guid")
+          val flashVersion = row.getAs[String]("flashVersion")
+          val jre = row.getAs[String]("jre")
+          val sr = row.getAs[String]("sr")
+          val sc = row.getAs[String]("sc")
+          val geographic = row.getAs[Int]("geographic")
+          val category = row.getAs[Int]("category")
+
+          val put = new Put(Bytes.toBytes(timeCreate))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("timeCreate"), Bytes.toBytes(timeCreate))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("cookieCreate"), Bytes.toBytes(cookieCreate))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("browserCode"), Bytes.toBytes(browserCode))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("browserVer"), Bytes.toBytes(browserVer))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("osCode"), Bytes.toBytes(osCode))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("osVer"), Bytes.toBytes(osVer))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("ip"), Bytes.toBytes(ip))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("locId"), Bytes.toBytes(locId))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("domain"), Bytes.toBytes(domain))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("siteId"), Bytes.toBytes(siteId))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("cId"), Bytes.toBytes(cId))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("path"), Bytes.toBytes(path))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("referer"), Bytes.toBytes(referer))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("guid"), Bytes.toBytes(guid))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("flashVersion"), Bytes.toBytes(flashVersion))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("jre"), Bytes.toBytes(jre))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("sr"), Bytes.toBytes(sr))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("sc"), Bytes.toBytes(sc))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("geographic"), Bytes.toBytes(geographic))
+          put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("category"), Bytes.toBytes(category))
+
+          puts.add(put)
+          table.put(puts)
+          puts.clear()
+        }
+      } finally {
+        hbaseConnection.close()
+      }
+    })
+  }
 
   private def readHBaseThenWriteToHDFS(): Unit = {
     println("----- Read person:info table to dataframe then analysis and write result to HDFS ----")
@@ -439,14 +427,14 @@ object SparkHBase {
     centroids.foreach(println)
 
     val predictions = model.transform(dataWithFeatures)
-    predictions.show(30000)
+    predictions.show(30)
   }
 
   def main(args: Array[String]): Unit = {
     //    createDataFrameAndPutToHDFS()
-    //    readHDFSThenPutToHBase()
+    readHDFSThenPutToHBase()
     //    readHBaseThenWriteToHDFS()
     //    datalogEx()
-    kmeanEx(3)
+    //    kmeanEx(3)
   }
 }
