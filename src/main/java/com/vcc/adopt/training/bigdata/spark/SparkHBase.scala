4882368d463be20c09b6ce31d46457c7117a1482
Duy Thai <sb23092002@gmail.com>
2024-02-26 14:35:38 +0700

    Debug

--- src/main/java/com/vcc/adopt/training/bigdata/spark/SparkHBase.scala
+++ src/main/java/com/vcc/adopt/training/bigdata/spark/SparkHBase.scala
@@ -354,9 +354,10 @@ object SparkHBase {
         val table = hbaseConnection.getTable(TableName.valueOf("bai4", "pageviewlog"))
         try {
           rows.map(row => {
-            val get = new Get(Bytes.toBytes(row.getAs[Long]("guid")))
+            val get = new Get(Bytes.toBytes(Option(row.getAs[java.sql.Timestamp]("cookieCreate")).map(_.getTime).getOrElse(0L)))
+            get.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("guid"))
             get.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("ip")) // mặc định sẽ lấy ra tất cả các cột, dùng lệnh này giúp chỉ lấy cột age
-            (row.getAs[Long]("guid"), Bytes.toLong(table.get(get).getValue(Bytes.toBytes("cf"), Bytes.toBytes("ip"))))
+            (Bytes.toLong(table.get(get).getValue(Bytes.toBytes("cf"), Bytes.toBytes("guid"))), Bytes.toLong(table.get(get).getValue(Bytes.toBytes("cf"), Bytes.toBytes("ip"))))
           })
         } finally {
           hbaseConnection.close()
